234b09e9285d62e5b65c11bbe7132641734e4d41
Hayden <lifesh92@gmail.com>
2020-12-10 16:04:11 +0900

    버튼 Styled Component 완료

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,80 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import "./button.css";
+import { Primary } from "./Button.stories";
 
+const StyeldButton = styled.div`
+  font-family: "Noto Sans CJK KR", "Nunito Sans", "Helvetica Neue", Helvetica,
+    Arial, sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  text-align: center;
+  display: inline-block;
+  //버튼 Fontcolor
+  .primary {
+    color: #232527;
+    background-color: #ffc847;
+  }
+  .disabled {
+    color: #b4b8bc;
+    background-color: #dee2e6;
+  }
+  .blank {
+    color: #ff6d6f;
+    background-color: #ffffff;
+    border: 1px solid #dee2e6;
+    box-sizing: border-box;
+  }
+  .gray {
+    color: #4c5154;
+    background-color: #edf0f3;
+  }
+  .dark {
+    color: #ffffff;
+    background-color: #3a3d40;
+  }
+
+  //버튼 Size
+  .tiny {
+    border-radius: 4px;
+    font-size: 14px;
+    padding: 8px 20px;
+  }
+  .small {
+    font-size: 14px;
+    padding: 10px 30px;f
+    border-radius: 8px;
+  }
+  .medium {
+    font-size: 16px;
+    padding: 10px 52px;
+    border-radius: 8px;
+  }
+  .large {
+    font-size: 16px;
+    padding: 14px 79px;
+    border-radius: 8px;
+  }
+  .giant {
+    font-size: 16px;
+    padding: 18px 169px;
+    border-radius: 8px;
+  }
+
+  //버튼 Roundness
+  .round {
+    border-radius: 8px;
+  }
+
+  .round1 {
+    border-radius: 41px;
+  }
+
+  .round2 {
+    border-radius: 55px;
+  }
+`;
 /**
  * Primary UI component for user interaction
  */
@@ -16,24 +88,17 @@ export const Button = ({
   ...props
 }) => {
   const mode = status ? "maroma-button--primary" : "maroma-button--disabled";
+  console.log(size);
+  console.log(status);
   return (
     <div>
-      <asdf></asdf>
-      <button
-        type="button"
-        className={[
-          "maroma-button",
-          `maroma-button--${status}`,
-          `maroma-button--${size}`,
-          `maroma-button--${border}`,
-          `maroma-button--${fontcolor}`,
-          mode,
-        ].join(" ")}
-        style={backgroundColor && { backgroundColor }}
-        {...props}
-      >
-        {label}
-      </button>
+      <StyeldButton>
+        <StyeldButton
+          className={[`${size}`, `${status}`, `${border}`].join(" ")}
+        >
+          {label}
+        </StyeldButton>
+      </StyeldButton>
     </div>
   );
 };
@@ -42,46 +107,26 @@ Button.propTypes = {
   /**
    * 상태에 따른 버튼 종류
    */
-  status: PropTypes.oneOf(["Primary", "Blank", "Disabled", "Dark", "Gray"]),
-  /**
-   * 배경 색상 종류
-   */
-  backgroundColor: PropTypes.oneOf([
-    "Primary",
-    "Blank",
-    "Disabled",
-    "Dark",
-    "Gray",
-  ]),
-  /**
-   * 폰트 색상 종류
-   */
-  fontcolor: PropTypes.oneOf([
-    "blackFont",
-    "darkFont",
-    "grayFont",
-    "primaryFont",
-    "whiteFont",
-  ]),
+  status: PropTypes.oneOf(["primary", "blank", "disabled", "dark"]),
   /**
    * 버튼 사이즈 선택
    */
-  size: PropTypes.oneOf(["Tiny", "Small", "Medium", "Large", "Giant"]),
+  size: PropTypes.oneOf(["tiny", "small", "medium", "large", "giant"]),
   /**
    * 버튼에 들어가는 내용 입력
    */
   label: PropTypes.string.isRequired,
   /**
-   * Border 유무 선택
+   * 버튼 모양 선택
    */
-  border: PropTypes.oneOf(["Border", "noBorder"]),
+  border: PropTypes.oneOf(["round", "round1", "round2"]),
 
   onClick: PropTypes.func,
 };
 
 Button.defaultProps = {
   /*backgroundColor: null,*/
-  status: false,
+  status: "primary",
   size: "medium",
   onClick: undefined,
 };
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -14,34 +14,34 @@ const Template = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  status: "Primary",
+  status: "primary",
   label: "button",
-  border: "noBorder",
-  size: "Small",
+  border: "round",
+  size: "small",
 };
 
-export const Disabled = Template.bind({});
-Disabled.args = {
-  status: "Disabled",
-  label: "button",
-  border: "noBorder",
-  size: "Small",
-};
+// export const Disabled = Template.bind({});
+// Disabled.args = {
+//   status: "Disabled",
+//   label: "button",
+//   border: "noBorder",
+//   size: "Small",
+// };
 
-export const Dark = Template.bind({});
-Dark.args = {
-  fontcolor: "whiteFont",
-  backgroundColor: "rgba(58,61,64,1)",
-  label: "button",
-  border: "noBorder",
-  size: "Small",
-};
+// export const Dark = Template.bind({});
+// Dark.args = {
+//   fontcolor: "whiteFont",
+//   backgroundColor: "rgba(58,61,64,1)",
+//   label: "button",
+//   border: "noBorder",
+//   size: "Small",
+// };
 
-export const Blank = Template.bind({});
-Blank.args = {
-  size: "Small",
-  fontcolor: "primaryFont",
-  label: "button",
-  border: "Border",
-  backgroundColor: "rgba(0,0,0,0)",
-};
+// export const Blank = Template.bind({});
+// Blank.args = {
+//   size: "Small",
+//   fontcolor: "primaryFont",
+//   label: "button",
+//   border: "Border",
+//   backgroundColor: "rgba(0,0,0,0)",
+// };
